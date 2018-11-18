1f26896f67e68c8f26e54e56ca2080f766dc81e6
Gioele Bigini <gioele.bigini@outlook.com>
2018-11-18 12:27:21 +0100

    Manager now can handle Ctrl-C

--- zookeeper_project/manager/src/main/java/master/eit/Main.java
+++ zookeeper_project/manager/src/main/java/master/eit/Main.java
@@ -4,14 +4,21 @@ import org.apache.log4j.Logger;
 import org.apache.zookeeper.KeeperException;
 
 import java.io.IOException;
+import java.util.concurrent.Semaphore;
 
 
 public class Main {
     private static final Logger logger = LogManager.getLogger("Main Class");
 
     public static void main(String[] args) {
-
         Manager manager = null;
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                System.out.println("\nYou killed me :'(");
+            }
+        });
+
         try {
             manager = new Manager("localhost:2181");
         } catch (IOException | InterruptedException | KeeperException e) {
@@ -21,13 +28,17 @@ public class Main {
         thread.start();
         logger.info("The Manager has caught up with requests and started up.");
 
+        while (true) {
 
-
-        while (true){
-            //TODO: Exit this while with a condition and not only with an exception.
-
-            //TODO: every 30 seconds (?) create a state of the system and save it in the manager
+            try {
+                System.out.println("Ctrl-C to shutdown the Manager");
+                Thread.sleep(30000);
+            } catch(InterruptedException e) {
+                System.out.println("I cannot sleep, something woke me up");
+            }
 
         }
+
+
     }
 }
